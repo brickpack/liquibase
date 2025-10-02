6a08399dd718915f41c0b6b4b15421fe6ef39574
Dave Birkbeck <brickpack@gmail.com>
2025-10-01 22:47:05 -0600

    Refactor database name from the_db to thedb
    Renamed all references to use single word 'thedb':
    - changelog-postgres-the_db.xml -> changelog-postgres-thedb.xml
    - db/changelog/postgres/the_db/ -> db/changelog/postgres/thedb/
    - Updated user GRANT statements: the_db -> thedb
    - Updated all file paths in changelog XML
    
    Database will now be created as 'thedb' by the workflow.

--- db/changelog/postgres/thedb/users/001-create-users.sql
+++ db/changelog/postgres/thedb/users/001-create-users.sql
@@ -3,7 +3,7 @@
 --changeset db-admin:001
 --comment: Create read-write user for application
 CREATE USER app_readwrite WITH PASSWORD 'CHANGE_ME_TEMP_PASSWORD';
-GRANT CONNECT ON DATABASE the_db TO app_readwrite;
+GRANT CONNECT ON DATABASE thedb TO app_readwrite;
 GRANT USAGE ON SCHEMA public TO app_readwrite;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_readwrite;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO app_readwrite;
@@ -15,7 +15,7 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO app_read
 --changeset db-admin:002
 --comment: Create read-only user for reporting
 CREATE USER app_readonly WITH PASSWORD 'CHANGE_ME_TEMP_PASSWORD';
-GRANT CONNECT ON DATABASE the_db TO app_readonly;
+GRANT CONNECT ON DATABASE thedb TO app_readonly;
 GRANT USAGE ON SCHEMA public TO app_readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO app_readonly;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO app_readonly;
