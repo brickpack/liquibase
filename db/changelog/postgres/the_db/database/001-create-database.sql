5756ef58d53b2a1bb0edff78bdea96d2b274b1f0
Dave Birkbeck <brickpack@gmail.com>
2025-10-01 22:42:25 -0600

    Remove unnecessary database creation changeset
    The workflow automatically creates the database based on the changelog filename.
    Database 'the_db' will be created by create-database.sh script.

--- db/changelog/postgres/the_db/database/001-create-database.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset db-admin:000
---comment: Create the_db database
---preconditions onFail:MARK_RAN
---precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM pg_database WHERE datname = 'the_db'
-CREATE DATABASE the_db;
